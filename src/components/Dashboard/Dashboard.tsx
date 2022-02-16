9e619e830aa477a98a19d9001aa4eaa977e91230
terlam <max.rel@gmail.com>
2022-02-16 08:51:26 -0600

    update link styling

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,5 +1,20 @@
-import React from "react";
-
+import React, {useState} from "react";
+import { Drawer as MUIDrawer,
+    ListItem, 
+    List, 
+    ListItemIcon, 
+    ListItemText, 
+    Theme,
+    useTheme, 
+    makeStyles, 
+    createStyles,
+    AppBar,
+    Toolbar,
+    IconButton,
+    Typography,
+    Divider,
+    Button
+} from "@mui/material";
 export const Dashboard = () => {
     return (
         <div>
